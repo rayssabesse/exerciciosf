893b8f7191360f31d561be004f84aff56d4324f9
Rayssa Besse <53128035+rayssabesse@users.noreply.github.com>
2021-05-20 13:56:19 -0300

    Add files via upload

---
+++ ex3/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+
+namespace ex3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine("Qual o nome do produto?");
+            Console.ForegroundColor = ConsoleColor.White;
+            string name = Console.ReadLine();
+
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine("Quantos produtos deseja comprar?");
+            Console.ForegroundColor = ConsoleColor.White;
+            int q = int.Parse(Console.ReadLine());
+
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine("Qual o valor do produto?");
+            Console.ForegroundColor = ConsoleColor.White;
+            double p = double.Parse(Console.ReadLine());
+
+            double t1 = q * p;
+
+            double d = 0;
+
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine("O valor do produto é de: R$" + t1);
+            Console.ForegroundColor = ConsoleColor.White;
+
+            if (q <= 5)
+            {
+                d = ((p * 2) / 100);
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("O valor do desconto é de: R$" + d);
+                Console.ForegroundColor = ConsoleColor.White;
+            }
+
+            if (q > 5 && q <= 10)
+            {
+                d = ((p * 3) / 100);
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("O valor do desconto é de: R$" + d);
+                Console.ForegroundColor = ConsoleColor.White;
+            }
+            if (q > 10)
+            {
+                d = ((p * 5) / 100);
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("O valor do desconto é de: R$" + d);
+                Console.ForegroundColor = ConsoleColor.White;
+            }
+
+            double t = t1 - d;
+
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine("O total a pagar é de: R$" + t);
+            Console.ForegroundColor = ConsoleColor.White;
+        }
+    }
+}
---
+++ ex4/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace ex4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine("Digite 10 números: ");
+            Console.ForegroundColor = ConsoleColor.White;
+            int[] numbers = new int[10];
+
+            for (var i = 0; i < numbers.Length; i++)
+            {
+                numbers[i] = int.Parse(Console.ReadLine());
+            }
+
+            Array.Sort(numbers);
+
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine("O menor número é: " + numbers[0]);
+            Console.WriteLine("O menor número é: " + numbers[9]);
+            Console.ForegroundColor = ConsoleColor.White;
+        }
+    }
+}
