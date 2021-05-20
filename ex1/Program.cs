f885d722383b64b1752e599851f53dca2ce778da
Rayssa Besse <rayssabesse@gmail.com>
2021-05-19 23:33:42 -0300

    first commit

---
+++ ex1/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace ex1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int year = DateTime.Now.Year;
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine("Em qual ano você nasceu?");
+            Console.ForegroundColor = ConsoleColor.White;
+            int byear = int.Parse(Console.ReadLine());
+
+            int age = year - byear;
+
+            if (age >= 18)
+            {
+                Console.ForegroundColor = ConsoleColor.Blue;
+                Console.WriteLine("Você tem " + age + " anos. Você DEVE votar.");
+                Console.ForegroundColor = ConsoleColor.White;
+            }
+            if (age >= 16 && age < 18)
+            {
+                Console.ForegroundColor = ConsoleColor.Blue;
+                Console.WriteLine("Você tem " + age + " anos. Você PODE votar.");
+                Console.ForegroundColor = ConsoleColor.White;
+            }
+            if (age < 16)
+            {
+                Console.ForegroundColor = ConsoleColor.Blue;
+                Console.WriteLine("Você tem " + age + " anos. Você NÃO PODE votar.");
+                Console.ForegroundColor = ConsoleColor.White;
+            }
+
+
+        }
+    }
+}
---
+++ ex2/Program.cs
@@ -0,0 +1,77 @@
+﻿using System;
+
+namespace ex2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine(
+                "Qual o combustível que deseja?\n" +
+                "Digite 'A' para álcool\n" +
+                "Digite 'G' para gasolina"
+            );
+            Console.ForegroundColor = ConsoleColor.White;
+            string c = Console.ReadLine();
+
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine(
+                "\nQuantos litros deseja abastecer?"
+            );
+            Console.ForegroundColor = ConsoleColor.White;
+            double l = double.Parse(Console.ReadLine());
+
+            double alcool = 4.90;
+            double gasolina = 5.30;
+
+            switch (c)
+            {
+                case "A":
+                    if (l < 20)
+                    {
+                        double d = l * ((alcool * 3) / 100);
+                        double sd = l * alcool;
+                        double vt = sd - d;
+
+                        Console.ForegroundColor = ConsoleColor.Red;
+                        Console.WriteLine("\nO valor total é de: R$" + vt);
+                        Console.ForegroundColor = ConsoleColor.White;
+                    }
+                    if (l >= 20)
+                    {
+                        double d = l * ((alcool * 5) / 100);
+                        double sd = l * alcool;
+                        double vt = sd - d;
+
+                        Console.ForegroundColor = ConsoleColor.Red;
+                        Console.WriteLine("\nO valor total é de: R$" + vt);
+                        Console.ForegroundColor = ConsoleColor.White;
+                    }
+                    break;
+                case "G":
+                    if (l < 20)
+                    {
+                        double d = l * ((gasolina * 4) / 100);
+                        double sd = l * gasolina;
+                        double vt = sd - d;
+
+                        Console.ForegroundColor = ConsoleColor.Red;
+                        Console.WriteLine("\nO valor total é de: R$" + vt);
+                        Console.ForegroundColor = ConsoleColor.White;
+                    }
+                    if (l >= 20)
+                    {
+                        double d = l * ((gasolina * 5) / 100);
+                        double sd = l * gasolina;
+                        double vt = sd - d;
+
+                        Console.ForegroundColor = ConsoleColor.Red;
+                        Console.WriteLine("\nO valor total é de: R$" + vt);
+                        Console.ForegroundColor = ConsoleColor.White;
+                    }
+                    break;
+            }
+        }
+    }
+}
